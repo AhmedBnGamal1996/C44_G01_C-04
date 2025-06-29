ed73b3b6ed12c294941abe0dfe6d3b086ec9b75d
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-06-29 20:59:13 +0300

    Add project files.

---
+++ Demo_C44_G01_C#04/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_C44_G01_C_04
+{
+    internal class Person
+    {
+    }
+}
