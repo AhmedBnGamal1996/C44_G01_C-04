11c366eab14a841dc8c3eee695477aecc50dc8e0
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-07-01 23:06:56 +0300

    Question 1

---
+++ Assignment C44 _ G01 _ C#04/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+
+namespace Demo_C44_G01_C_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Question 1 
+            /*1 - Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+                Example(1)
+                Input: 12
+                Output: Yes
+                Example(2)
+                Input: 9
+                Output: No
+            */
+
+            //Console.WriteLine("please enter a number ..");
+
+            //int number;
+
+            //bool isParsed = int.TryParse(Console.ReadLine(), out number);
+
+
+            //if (number%3 == 0 && number%4 == 0 )    
+            //{
+            //    Console.WriteLine("Yes");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+}
