2b86cd6c1aaebd3dcef3cca32a581fbcca9a8bc6
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-07-01 09:50:13 +0300

    Video 10 1D Array

--- Demo_C44_G01_C#04/Program.cs
+++ Demo_C44_G01_C#04/Program.cs
@@ -655,7 +655,85 @@ namespace Demo_C44_G01_C_04
 
 
 
+            #region Video 10 1D Array 
+            //Array arr = new Array();  // can not instance 
+            //int[] numbers;
+            // allocate 4 bytes in stack 
+            //numbers = new int[4] {1,2,3,4};
+            //numbers = new int[] {1,2,3,4};
+            //int[] numbers = [ 1, 2, 3, 4, 5 ];
 
+            //int[] numbers = new int[3];
+            //Array.Fill(numbers, 5);
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers[2]);
+
+            //Array arr = Array.CreateInstance(typeof(int), 5);
+            //arr.SetValue(10, 0);
+            //arr.SetValue(20, 1);
+            //arr.SetValue(30, 2);
+            //arr.SetValue(40, 3);
+            //arr.SetValue(50, 4);
+            //Console.WriteLine(arr.GetValue(0));
+
+            //object[] data = new object[3] { 1, "Mariem", true };
+            //foreach (var item in data)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4 };
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    console.writeline(numbers[i]);
+            //}
+
+            //int[] numbers = new int [5];
+            //for (int i = 0; i < numbers.Length;)
+            //{
+            //    Console.WriteLine($"Please enter the array elemeny {i+1}");
+            //    bool isParsed = int.TryParse(Console.ReadLine(), out numbers[i]);
+            //    if (isParsed)
+            //    {
+            //        ++i; 
+            //    }
+                
+            //}
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            // New
+            //1] allocate required at heap [ 4 * 4 = 16 bytes ] 
+            //2] Inialize default value [0] 
+            //3] call user defined constracor if exist
+            //4] assign reference in stack to instance in heap 
+
+            //char[] characters = new char[3] ;
+
+            //// Default Value for Char type = \0 
+
+            //char m = default;
+            //Console.WriteLine(m);
+            //Console.WriteLine((int)m);  // space = 0 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
 
 
 
